45b0d5b773ab77b8c27074d1404aadaa4b6ad0aa
Dilermando Barbosa (Extern, T-Systems) <Dilermando.Barbosa@t-systems.com.br>
2018-03-08 11:24:51 -0300

    fix author description

--- Example/EZLottieExtension/ViewController.swift
+++ Example/EZLottieExtension/ViewController.swift
@@ -7,18 +7,26 @@
 //
 
 import UIKit
+import Lottie
+import EZLottieExtension // import EZLottieExtension to your project
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var myButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        myButton.addLottieView(LOTAnimationView(name: "done_button"), scale: 4)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    
+    @IBAction func myButtonPress(_ sender: Any) {
+        myButton.playAnimation()
     }
+    
 
 }
 
